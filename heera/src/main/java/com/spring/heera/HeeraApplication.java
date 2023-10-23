2dbfd148177fa5e0170ee309c58bf5e268ec938c
duck-ach <djagmlfk@naver.com>
2023-10-23 23:32:43 +0900

    프로젝트 추가, yaml 수정

---
+++ heera/src/main/java/com/spring/heera/HeeraApplication.java
@@ -0,0 +1,13 @@
+package com.spring.heera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeeraApplication.class, args);
+	}
+
+}
---
+++ heera/src/test/java/com/spring/heera/HeeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.heera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
